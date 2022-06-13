3deb6bcafb2e3864713432f33942d3b1021d8064
adyluna <adylunaleite@gmail.com>
2022-06-13 16:49:00 -0300

    exercicios 1 e 2 ok

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercise-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDoList from './ToDoList';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {ToDoList.map((elem) => Task(elem))}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
